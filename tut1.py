0a919e0718c69ac7c0b9fe8330cc540c56376884
shubhhi-s <118956332+shubhhi-s@users.noreply.github.com>
2023-03-08 12:29:00 +0530

    Add files via upload
    Compilation of CS419 files

---
+++ tut1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+import torch
+%pip install -U git+https://github.com/szagoruyko/pytorchviz.git@master
+from torchviz import make_dot
